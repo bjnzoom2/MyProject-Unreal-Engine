1908df4c0c935e7a4fc2d8edeb81e73730cacdbb
bjnzoom2 <bjnzoom2@gmail.com>
2025-11-16 20:47:59 +0800

    Learnt UCLASS, UPROPERTY and UFUNCTION

---
+++ Source/MyProject/Public/PlayerTestCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TestCharacter.h"
+#include "PlayerTestCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT_API APlayerTestCharacter : public ATestCharacter
+{
+	GENERATED_BODY()
+	
+};
